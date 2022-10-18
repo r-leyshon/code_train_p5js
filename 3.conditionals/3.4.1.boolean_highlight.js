24d79dc40e50b23c50d236d4d67f4c3576f7d97c
r-leyshon <richard.leyshon@ons.gov.uk>
2022-10-18 06:55:07 +0100

    feat: Add switch light behaviour

--- 3.conditionals/3.4.1.boolean_highlight.js
+++ 3.conditionals/3.4.1.boolean_highlight.js
@@ -1,24 +1,49 @@
+let on = false;
+let insideRect;
+
 function setup() {
-    createCanvas(600, 400);
-  }
-  
-  function draw() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  // light in the room
+  if (on) {
+    background(255, 255, 0);
+  } else {
     background(0);
-  
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    rectMode(CENTER);
-    // conditional colour
-    if (mouseX > 250 && mouseX < 350 && mouseY < 250 && mouseY > 150) {
-      fill(255, 0, 200);
-  
-      // hold to highlight
-      if (mouseIsPressed) {
-        fill(0, 255, 0);
-      }
-    }
-  
-    rect(300, 200, 100, 100);
   }
-  
+
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  rectMode(CENTER);
+
+  // check if mouse is inside rectangle
+  if (mouseX > 250 && mouseX < 350 && mouseY < 250 && mouseY > 150) {
+    insideRect = true;
+  } else {
+    insideRect = false;
+  }
+
+  // conditional colour button
+  if (insideRect) {
+    fill(255, 0, 200);
+
+    // hold to highlight
+    if (mouseIsPressed) {
+      fill(0, 255, 0);
+    }
+  }
+
+  rect(300, 200, 100, 100);
+}
+
+// as we want a switch behaviour
+// we change state of program
+// outside of draw
+function mousePressed() {
+  if (insideRect) {
+    // toggle a boolean with the not operator
+    on = !on;
+  }
+}
