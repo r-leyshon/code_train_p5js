4a537e32b5b06afb0be631c2e0274a61760182f4
r-leyshon <richard.leyshon@ons.gov.uk>
2022-10-28 10:49:02 +0100

    feat: Stage chapter 3 for website

--- 3.conditionals/3.2.bouncing-ball/sketch.js
+++ 3.conditionals/3.2.bouncing-ball/sketch.js
@@ -1,33 +1,34 @@
 let x = 0;
-let y = 0;
-let xspeed = 3;
-let yspeed = 3;
+let y = 5;
+let xspeed = 60;
+let yspeed = 6;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(1600, 900);
   background(0);
 }
 
 function draw() {
   // x pos
   if (x < 0) {
-    xspeed = +3;
+    xspeed = -1*xspeed;
   }
   if (x > width) {
-    xspeed = -3;
+    xspeed = -1*xspeed;
   }
   x += xspeed;
 
   // ypos
   if (y > height) {
-    yspeed = -3;
+    yspeed = -1 * yspeed;
   }
   if (y < 0) {
-    yspeed = +3;
+    yspeed = -1*yspeed;
   }
   y += yspeed;
 
   // dynamic fill
-  fill(x, x - y, y);
+  noStroke();
+  fill(x, x - y, y, 80);
   ellipse(x, y, 100, 100);
 }
