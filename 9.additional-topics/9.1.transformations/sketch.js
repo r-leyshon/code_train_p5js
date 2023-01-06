61492df0feebc8a648db4be42a33267666300418
r-leyshon <richard.leyshon@ons.gov.uk>
2023-01-06 09:54:02 +0000

    feat: 9.1 - translate, push, pop

---
+++ 9.additional-topics/9.1.transformations/sketch.js
@@ -0,0 +1,37 @@
+let angle = 0;
+let x = 50;
+let y = 50;
+
+function setup() {
+  createCanvas(400, 400);
+  // as opposed to radians
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+function draw() {
+  // every time through draw translations
+  // rest back to 0,0
+  background(0);
+
+  // push can be used to save the
+  // state of the translation
+  push();
+
+  // move the origin
+  translate(x, y);
+  rotate(angle);
+
+  fill(170, 74, 68);
+  rect(0, 0, 100, 50);
+
+  // pop can be used to restore the
+  // state of the translation
+  pop();
+
+  translate(300, 300);
+  rotate(-angle);
+  fill(240, 220, 130);
+  rect(0, 0, 100, 50);
+  angle = angle + 5;
+}
