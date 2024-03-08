1b7272db76b26f26cee85001abe56f859dd6f4b9
Richard Leyshon <49126943+r-leyshon@users.noreply.github.com>
2024-03-08 06:54:18 +0000

    feat: Add random walker

---
+++ nature-of-code/01-random-walker/sketch.js
@@ -0,0 +1,32 @@
+let x = 800;
+let y = 350;
+let step_size = 5;
+
+function setup() {
+  createCanvas(1600, 700);
+  background(0);
+}
+
+function draw() {
+  direction = int(random(4));
+  if (direction == 0) {
+    x -= step_size;
+  } else if (direction == 1) {
+    x += step_size;
+  } else if (direction == 2) {
+    y -= step_size;
+  } else {
+    y += step_size;
+  }
+  x = constrain(x, step_size, width - step_size);
+  y = constrain(y, step_size, height - step_size);
+  r = (x / width) * 255;
+  g = (y / height) * 255;
+  // Invert b otherwise becomes black in topleft corner
+  b = (1 - (r/255 * g/255)) * 255;
+
+  stroke(r, g, b, 50);
+  strokeWeight(step_size);
+  point(x, y);
+
+}
