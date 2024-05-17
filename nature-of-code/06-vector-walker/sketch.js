d18db321e96e0eccd80418b578d7b8c35da36648
Richard Leyshon <49126943+r-leyshon@users.noreply.github.com>
2024-05-17 14:21:41 +0100

    feat: Create walkers on mouse click

--- nature-of-code/06-vector-walker/sketch.js
+++ nature-of-code/06-vector-walker/sketch.js
@@ -1,12 +1,13 @@
 let walkers = [];
+let step_size = 2;
+
+function mousePressed() {
+  append(walkers, new Walker(mouseX, mouseY, step_size));
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  for (let i = 0; i < 3; i++) {
-    inv_col = random(["r", "g", "b"]);
-    walkers[i] = new Walker(width / 2, height / 2, 3, inv_col);
-  }
+  background(150);
 }
 
 function draw() {
@@ -14,7 +15,4 @@ function draw() {
     walkers[i].walk();
     walkers[i].show();
   }
-  
-  // col = color(random(255), random(255), random(255));
-  // print(col)
 }
--- nature-of-code/06-vector-walker/walker.js
+++ nature-of-code/06-vector-walker/walker.js
@@ -1,41 +1,34 @@
 class Walker {
-    constructor(x, y, step_size, invert_colour) {
-      this.step_size = step_size;
-      this.pos = createVector(x, y);
-      this.invert_colour = invert_colour;
-    }
-  
-    walk() {
-      this.pos.x = constrain(
-        this.pos.x + random(this.step_size * -1, this.step_size),
-        this.step_size,
-        width - this.step_size
-      );
-      this.pos.y = constrain(
-        this.pos.y + random(this.step_size * -1, this.step_size),
-        this.step_size,
-        height - this.step_size
-      );
-    }
-  
-    show() {
-      this.r = (this.pos.x / width) * 255;
-      this.g = (this.pos.y / height) * 255;
-      // Invert b otherwise becomes black in topleft corner
-      this.b = (1 - ((this.r / 255) * this.g) / 255) * 255;
-  
-      if (this.invert_colour == "r") {
-        this.r = 255 - this.r;
-      } else if (this.invert_colour == "g") {
-        this.g = 255 - this.g;
-      } else {
-        this.b = 255 - this.b;
-      }
-  
-      strokeWeight(7);
-      noStroke();
-      fill(this.r, this.g, this.b, 125);
-      ellipse(this.pos.x, this.pos.y, 5);
-    }
+  constructor(x, y, step_size) {
+    this.step_size = step_size;
+    this.pos = createVector(x, y);
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
   }
-  
+
+  walk() {
+    this.pos.x = constrain(
+      this.pos.x + random(this.step_size * -1, this.step_size),
+      this.step_size,
+      width - this.step_size
+    );
+    this.pos.y = constrain(
+      this.pos.y + random(this.step_size * -1, this.step_size),
+      this.step_size,
+      height - this.step_size
+    );
+  }
+
+  show() {
+    let r,
+      g,
+      b = 0;
+    r = (this.pos.x / width) * this.r;
+    g = (this.pos.y / height) * this.g;
+    b = constrain(r / g, 0.0, 1.0) * this.b;
+    noStroke();
+    fill(r, g, b, 40);
+    ellipse(this.pos.x, this.pos.y, 5);
+  }
+}
